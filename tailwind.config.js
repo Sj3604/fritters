80e7af312741803a10f6c338b4ec64850cf1a09e
Shagun Jain <jainshagun23@gmail.com>
2025-09-07 23:48:33 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  purge: ['./index.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        potato: '#D9A86A',
+        crispy: '#C9A46D',
+        highlight: '#FFDD57',
+      },
+      spacing: {
+        '128': '32rem',
+        '144': '36rem',
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
